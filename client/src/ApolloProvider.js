033a5baf81302b61ee990b745d866ccdaf73b75c
Lanreath <tyfei00gmail.com>
2021-05-21 23:16:07 +0800

    Changes to be committed:
     Added comments resolvers
     Added client: Home, Login, Register pages

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,20 @@
+import React from "react";
+import App from "./App";
+
+import { ApolloProvider } from "@apollo/client/react";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:5000",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
