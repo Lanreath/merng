5dc5d87e1f16eb5ae590ed9fcac300b4e44ad161
Lanreath <tyfei00gmail.com>
2021-05-25 23:39:41 +0800

    Added delete button to PostCard

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Card, Icon, Label, Image } from "semantic-ui-react";
-import { Link } from 'react-router-dom';
-import moment from 'moment';
+import { Link } from "react-router-dom";
+import moment from "moment";
+
+import { AuthContext } from "../context/auth";
+import LikeButton from "./LikeButton";
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) {
+  const { user } = useContext(AuthContext);
 
-  function likePost(){
-    console.log('Like post!!')
-  };
-
-  function commentOnPost(){
-    console.log("Comment on post!!")
-  };
+  function likePost() {
+    console.log("Like post!!");
+  }
 
   return (
     <Card fluid>
@@ -24,29 +24,34 @@ function PostCard({
           src="https://react.semantic-ui.com/images/avatar/large/molly.png"
         />
         <Card.Header>{username}</Card.Header>
-        <Card.Meta as={Link} to ={`/posts/${id}`}>{moment(createdAt).fromNow(true)}</Card.Meta>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-      <Button as='div' labelPosition='right' onClick={likePost}>
-      <Button color='teal' basic>
-        <Icon name='heart' />
-      </Button>
-      <Label as='a' basic color='teal' pointing='left'>
-        {likeCount}
-      </Label>
-    </Button>
-    <Button as='div' labelPosition='right' onClick={commentOnPost}>
-      <Button color='blue' basic>
-        <Icon name='comments' />
-      </Button>
-      <Label as='a' basic color='blue' pointing='left'>
-        {commentCount}
-      </Label>
-    </Button>
+        <LikeButton post={{ id, likes, likeCount }} />
+        <Button as="div" labelPosition="right" as={Link} to={`/posts/${id}`}>
+          <Button color="blue" basic>
+            <Icon name="comments" />
+          </Button>
+          <Label as="a" basic color="blue" pointing="left">
+            {commentCount}
+          </Label>
+        </Button>
+        {user && user.username === username && (
+          <Button
+            as="div"
+            color="red"
+            floated="right"
+            onClick={() => console.log("Delete post")}
+          >
+            <Icon name="trash" style={{ margin: 0 }} />
+          </Button>
+        )}
       </Card.Content>
     </Card>
   );
 }
 
-export default PostCard
+export default PostCard;
