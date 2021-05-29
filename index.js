3f1204d15c25f03aeccb4aa0c1a176b7d89daa0e
Lanreath <tyfei00gmail.com>
2021-05-29 17:13:15 +0800

    Final deployment fixes for dependencies

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, PubSub } = require("apollo-server");
 const mongoose = require("mongoose");
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
+
+const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
   typeDefs,
@@ -15,8 +19,11 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log("MongoDB connected");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
   });
