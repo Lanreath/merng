d7e81094e1c923720f58875fbefdb68b756cebcf
Lanreath <tyfei00gmail.com>
2021-05-29 16:24:19 +0800

    Added popup descriptions for like, comment, and delete buttons

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -15,6 +15,7 @@ import moment from "moment";
 import DeleteButton from "../components/DeleteButton";
 import LikeButton from "../components/LikeButton";
 import { AuthContext } from "../context/auth";
+import MyPopup from "../utils/MyPopup";
 
 function SinglePost(props) {
   const postId = props.match.params.postId;
@@ -78,18 +79,20 @@ function SinglePost(props) {
               </Card.Content>
               <Card.Content extra>
                 <LikeButton user={user} post={{ id, likeCount, likes }} />
-                <Button
-                  as="div"
-                  labelPosition="right"
-                  onClick={() => console.log("Comment on post")}
-                >
-                  <Button basic color="blue">
-                    <Icon name="comments" />
+                <MyPopup content="Comment on post">
+                  <Button
+                    as="div"
+                    labelPosition="right"
+                    onClick={() => console.log("Comment on post")}
+                  >
+                    <Button basic color="blue">
+                      <Icon name="comments" />
+                    </Button>
+                    <Label basic color="blue" pointing="left">
+                      {commentCount}
+                    </Label>
                   </Button>
-                  <Label basic color="blue" pointing="left">
-                    {commentCount}
-                  </Label>
-                </Button>
+                </MyPopup>
                 {user && user.username === username && (
                   <DeleteButton postId={id} callback={deletePostCallback} />
                 )}
